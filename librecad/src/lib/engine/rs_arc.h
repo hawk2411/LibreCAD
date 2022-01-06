22422b80e92af4e31a242719dc2790bde5a91696
Harry Wagner <wagner-harry@web.de>
2022-01-06 20:39:49 +0100

    rs_arc.h and rs_arc.cpp auto format

--- librecad/src/lib/engine/rs_arc.h
+++ librecad/src/lib/engine/rs_arc.h
@@ -28,6 +28,7 @@
 #define RS_ARC_H
 
 #include "rs_atomicentity.h"
+
 class LC_Quadratic;
 
 
@@ -35,26 +36,27 @@ class LC_Quadratic;
  * Holds the data that defines an arc.
  */
 struct RS_ArcData {
-	RS_ArcData() = default;
-	~RS_ArcData() = default;
+    RS_ArcData() = default;
 
-	RS_ArcData(const RS_Vector& center,
-			   double radius,
-			   double angle1, double angle2,
-			   bool reversed);
+    ~RS_ArcData() = default;
 
-	void reset();
+    RS_ArcData(const RS_Vector &center,
+               double radius,
+               double angle1, double angle2,
+               bool reversed);
 
-	bool isValid() const;
+    void reset();
 
-	RS_Vector center;
-	double radius;
-	double angle1;
-	double angle2;
-	bool reversed;
+    bool isValid() const;
+
+    RS_Vector center;
+    double radius{};
+    double angle1{};
+    double angle2{};
+    bool reversed{};
 };
 
-std::ostream& operator << (std::ostream& os, const RS_ArcData& ad);
+std::ostream &operator<<(std::ostream &os, const RS_ArcData &ad);
 
 
 /**
@@ -64,28 +66,29 @@ std::ostream& operator << (std::ostream& os, const RS_ArcData& ad);
  */
 class RS_Arc : public RS_AtomicEntity {
 public:
-	RS_Arc()=default;
-    RS_Arc(RS_EntityContainer* parent,
-           const RS_ArcData& d);
+    RS_Arc() = default;
 
-	RS_Entity* clone() const override;
+    RS_Arc(RS_EntityContainer *parent,
+           const RS_ArcData &d);
+
+    RS_Entity *clone() const override;
 
     /**	@return RS2::EntityArc */
-	RS2::EntityType rtti() const override
-	{
+    RS2::EntityType rtti() const override {
         return RS2::EntityArc;
     }
+
     /** @return true */
-	bool isEdge() const override {
+    bool isEdge() const override {
         return true;
     }
 
     /** @return Copy of data that defines the arc. **/
-	RS_ArcData getData() const {
+    RS_ArcData getData() const {
         return data;
     }
 
-	RS_VectorSolutions getRefPoints() const override;
+    RS_VectorSolutions getRefPoints() const override;
 
     /** Sets new arc parameters. **/
     void setData(RS_ArcData d) {
@@ -93,145 +96,185 @@ public:
     }
 
     /** @return The center point (x) of this arc */
-	RS_Vector getCenter() const override {
+    RS_Vector getCenter() const override {
         return data.center;
     }
+
     /** Sets new center. */
-    void setCenter(const RS_Vector& c) {
+    void setCenter(const RS_Vector &c) {
         data.center = c;
     }
 
     /** @return The radius of this arc */
-	double getRadius() const override {
+    double getRadius() const override {
         return data.radius;
     }
+
     /** Sets new radius. */
     void setRadius(double r) {
         data.radius = r;
     }
 
     /** @return The start angle of this arc */
-	double getAngle1() const {
+    double getAngle1() const {
         return data.angle1;
     }
+
     /** Sets new start angle. */
     void setAngle1(double a1) {
         data.angle1 = a1;
     }
+
     /** @return The end angle of this arc */
     double getAngle2() const {
         return data.angle2;
     }
+
     /** Sets new end angle. */
     void setAngle2(double a2) {
         data.angle2 = a2;
     }
+
     /** get angle relative arc center*/
-    double getArcAngle(const RS_Vector& vp) {
+    double getArcAngle(const RS_Vector &vp) {
         return (vp - data.center).angle();
     }
+
     /**
      * @return Direction 1. The angle at which the arc starts at
      * the startpoint.
      */
-	double getDirection1() const override;
+    double getDirection1() const override;
+
     /**
      * @return Direction 2. The angle at which the arc starts at
      * the endpoint.
      */
-	double getDirection2() const override;
+    double getDirection2() const override;
 
     /**
      * @retval true if the arc is reversed (clockwise),
      * @retval false otherwise
      */
-	bool isReversed() const {
+    bool isReversed() const {
         return data.reversed;
     }
+
     /** sets the reversed status. */
     void setReversed(bool r) {
         data.reversed = r;
     }
 
     /** @return Start point of the entity. */
-	RS_Vector getStartpoint() const override;
+    RS_Vector getStartpoint() const override;
+
     /** @return End point of the entity. */
-	RS_Vector getEndpoint() const override;
-	std::vector<RS_Entity* > offsetTwoSides(const double& distance) const override;
+    RS_Vector getEndpoint() const override;
+
+    std::vector<RS_Entity *> offsetTwoSides(const double &distance) const override;
+
     /**
           * implementations must revert the direction of an atomic entity
           */
-	void revertDirection() override;
-	void correctAngles();//make sure angleLength() is not more than 2*M_PI
-	void moveStartpoint(const RS_Vector& pos) override;
-	void moveEndpoint(const RS_Vector& pos) override;
-	bool offset(const RS_Vector& position, const double& distance) override;
+    void revertDirection() override;
 
-	void trimStartpoint(const RS_Vector& pos) override;
-	void trimEndpoint(const RS_Vector& pos) override;
+    void correctAngles();//make sure angleLength() is not more than 2*M_PI
+    void moveStartpoint(const RS_Vector &pos) override;
 
-	RS2::Ending getTrimPoint(const RS_Vector& coord,
-									 const RS_Vector& trimPoint) override;
+    void moveEndpoint(const RS_Vector &pos) override;
+
+    bool offset(const RS_Vector &position, const double &distance) override;
+
+    void trimStartpoint(const RS_Vector &pos) override;
+
+    void trimEndpoint(const RS_Vector &pos) override;
+
+    RS2::Ending getTrimPoint(const RS_Vector &coord,
+                             const RS_Vector &trimPoint) override;
+
     /** choose an intersection to trim to based on mouse point */
-	RS_Vector prepareTrim(const RS_Vector& mousePoint,
-								  const RS_VectorSolutions& trimSol)override;
+    RS_Vector prepareTrim(const RS_Vector &mousePoint,
+                          const RS_VectorSolutions &trimSol) override;
 
-	void reverse() override;
+    void reverse() override;
 
-	RS_Vector getMiddlePoint() const override;
+    RS_Vector getMiddlePoint() const override;
+
     double getAngleLength() const;
-	double getLength() const override;
+
+    double getLength() const override;
+
     double getBulge() const;
 
-    bool createFrom3P(const RS_Vector& p1, const RS_Vector& p2,
-                      const RS_Vector& p3);
-    bool createFrom2PDirectionRadius(const RS_Vector& startPoint, const RS_Vector& endPoint,
+    bool createFrom3P(const RS_Vector &p1, const RS_Vector &p2,
+                      const RS_Vector &p3);
+
+    bool createFrom2PDirectionRadius(const RS_Vector &startPoint, const RS_Vector &endPoint,
                                      double direction1, double radius);
-    bool createFrom2PDirectionAngle(const RS_Vector& startPoint, const RS_Vector& endPoint,
-                                     double direction1, double angleLength);
-    bool createFrom2PBulge(const RS_Vector& startPoint, const RS_Vector& endPoint,
+
+    bool createFrom2PDirectionAngle(const RS_Vector &startPoint, const RS_Vector &endPoint,
+                                    double direction1, double angleLength);
+
+    bool createFrom2PBulge(const RS_Vector &startPoint, const RS_Vector &endPoint,
                            double bulge);
 
-	RS_Vector getNearestEndpoint(const RS_Vector& coord,
-										 double* dist = nullptr) const override;
-	RS_Vector getNearestPointOnEntity(const RS_Vector& coord,
-											  bool onEntity = true,
-											  double* dist = nullptr,
-											  RS_Entity** entity=nullptr) const override;
-	RS_Vector getNearestCenter(const RS_Vector& coord,
-									   double* dist = nullptr) const override;
-	RS_Vector getNearestMiddle(const RS_Vector& coord,
-									   double* dist = nullptr,
-                                       int middlePoints = 1
-									   ) const override;
-	RS_Vector getNearestDist(double distance,
-                                     const RS_Vector& coord,
-									 double* dist = nullptr) const override;
-	RS_Vector getNearestDist(double distance,
-									 bool startp) const override;
-	RS_Vector getNearestOrthTan(const RS_Vector& coord,
-                    const RS_Line& normal,
-					bool onEntity = false) const override;
-	RS_VectorSolutions getTangentPoint(const RS_Vector& point) const override;//find the tangential points seeing from given point
-	RS_Vector getTangentDirection(const RS_Vector& point) const override;
-	void move(const RS_Vector& offset) override;
-	void rotate(const RS_Vector& center, const double& angle) override;
-	void rotate(const RS_Vector& center, const RS_Vector& angleVector) override;
-	void scale(const RS_Vector& center, const RS_Vector& factor) override;
-	void mirror(const RS_Vector& axisPoint1, const RS_Vector& axisPoint2) override;
-	void moveRef(const RS_Vector& ref, const RS_Vector& offset) override;
-	void stretch(const RS_Vector& firstCorner,
-                         const RS_Vector& secondCorner,
-						 const RS_Vector& offset) override;
+    RS_Vector getNearestEndpoint(const RS_Vector &coord,
+                                 double *dist = nullptr) const override;
+
+    RS_Vector getNearestPointOnEntity(const RS_Vector &coord,
+                                      bool onEntity = true,
+                                      double *dist = nullptr,
+                                      RS_Entity **entity = nullptr) const override;
+
+    RS_Vector getNearestCenter(const RS_Vector &coord,
+                               double *dist = nullptr) const override;
+
+    RS_Vector getNearestMiddle(const RS_Vector &coord,
+                               double *dist = nullptr,
+                               int middlePoints = 1
+    ) const override;
+
+    RS_Vector getNearestDist(double distance,
+                             const RS_Vector &coord,
+                             double *dist = nullptr) const override;
+
+    RS_Vector getNearestDist(double distance,
+                             bool startp) const override;
+
+    RS_Vector getNearestOrthTan(const RS_Vector &coord,
+                                const RS_Line &normal,
+                                bool onEntity = false) const override;
+
+    RS_VectorSolutions
+    getTangentPoint(const RS_Vector &point) const override;//find the tangential points seeing from given point
+    RS_Vector getTangentDirection(const RS_Vector &point) const override;
+
+    void move(const RS_Vector &offset) override;
+
+    void rotate(const RS_Vector &center, const double &angle) override;
+
+    void rotate(const RS_Vector &center, const RS_Vector &angleVector) override;
+
+    void scale(const RS_Vector &center, const RS_Vector &factor) override;
+
+    void mirror(const RS_Vector &axisPoint1, const RS_Vector &axisPoint2) override;
+
+    void moveRef(const RS_Vector &ref, const RS_Vector &offset) override;
+
+    void stretch(const RS_Vector &firstCorner,
+                 const RS_Vector &secondCorner,
+                 const RS_Vector &offset) override;
 
     /** find the visible part of the arc, and call drawVisible() to draw */
-	void draw(RS_Painter* painter, RS_GraphicView* view, double& patternOffset) override;
+    void draw(RS_Painter *painter, RS_GraphicView *view, double &patternOffset) override;
+
     /** directly draw the arc, assuming the whole arc is within visible window */
-	void drawVisible(RS_Painter* painter, RS_GraphicView* view, double& patternOffset);
+    void drawVisible(RS_Painter *painter, RS_GraphicView *view, double &patternOffset);
 
-    friend std::ostream& operator << (std::ostream& os, const RS_Arc& a);
+    friend std::ostream &operator<<(std::ostream &os, const RS_Arc &a);
 
-	virtual void calculateBorders() override;
+    virtual void calculateBorders() override;
+
     /** return the equation of the entity
 for quadratic,
 
@@ -241,17 +284,18 @@ m0 x^2 + m1 xy + m2 y^2 + m3 x + m4 y + m5 =0
 for linear:
 m0 x + m1 y + m2 =0
 **/
-	virtual LC_Quadratic getQuadratic() const override;
+    virtual LC_Quadratic getQuadratic() const override;
+
     /**
      * @brief areaLineIntegral, line integral for contour area calculation by Green's Theorem
      * Contour Area =\oint x dy
      * @return line integral \oint x dy along the entity
      * \oint x dy = c_x r \sin t + \frac{1}{4}r^2\sin 2t +  \frac{1}{2}r^2 t
      */
-	virtual double areaLineIntegral() const override;
+    virtual double areaLineIntegral() const override;
 
 protected:
-	RS_ArcData data;
+    RS_ArcData data;
 };
 
 #endif
